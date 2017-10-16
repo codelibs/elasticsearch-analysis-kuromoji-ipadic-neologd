2313708f8f21b2b0a0314cc3a3bceee2dc65c28e
Shinsuke Sugaya <shinsuke@apache.org>
2017-10-16 23:19:20 +0900

    add test case

---
+++ src/test/java/org/codelibs/elasticsearch/kuromoji/neologd/analysis/PosConcatenationFilterFactoryTest.java
@@ -0,0 +1,151 @@
+package org.codelibs.elasticsearch.kuromoji.neologd.analysis;
+
+import static org.codelibs.elasticsearch.runner.ElasticsearchClusterRunner.newConfigs;
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
+
+import org.codelibs.elasticsearch.runner.ElasticsearchClusterRunner;
+import org.codelibs.elasticsearch.runner.net.Curl;
+import org.codelibs.elasticsearch.runner.net.CurlResponse;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.Settings.Builder;
+import org.elasticsearch.node.Node;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PosConcatenationFilterFactoryTest {
+
+    private ElasticsearchClusterRunner runner;
+
+    private int numOfNode = 1;
+
+    private File[] numberSuffixFiles;
+
+    private String clusterName;
+
+    @Before
+    public void setUp() throws Exception {
+        clusterName = "es-analysisja-" + System.currentTimeMillis();
+        runner = new ElasticsearchClusterRunner();
+        runner.onBuild(new ElasticsearchClusterRunner.Builder() {
+            @Override
+            public void build(final int number, final Builder settingsBuilder) {
+                settingsBuilder.put("http.cors.enabled", true);
+                settingsBuilder.put("http.cors.allow-origin", "*");
+                settingsBuilder.putArray("discovery.zen.ping.unicast.hosts", "localhost:9301-9310");
+            }
+        }).build(newConfigs().clusterName(clusterName).numOfNode(numOfNode).pluginTypes("org.codelibs.elasticsearch.kuromoji.neologd.KuromojiNeologdPlugin"));
+
+        numberSuffixFiles = null;
+    }
+
+    @After
+    public void cleanUp() throws Exception {
+        runner.close();
+        runner.clean();
+        if (numberSuffixFiles != null) {
+            for (File file : numberSuffixFiles) {
+                file.deleteOnExit();
+            }
+        }
+    }
+
+    @Test
+    public void test_basic() throws Exception {
+        numberSuffixFiles = new File[numOfNode];
+        for (int i = 0; i < numOfNode; i++) {
+            String confPath = runner.getNode(i).settings().get("path.conf");
+            numberSuffixFiles[i] = new File(confPath, "tags.txt");
+            updateDictionary(numberSuffixFiles[i], "名詞-形容動詞語幹\n名詞-サ変接続");
+        }
+
+        runner.ensureYellow();
+        Node node = runner.node();
+
+        final String index = "dataset";
+
+        final String indexSettings = "{\"index\":{\"analysis\":{"
+                + "\"filter\":{"
+                + "\"tag_concat_filter\":{\"type\":\"kuromoji_neologd_pos_concat\",\"tags_path\":\"tags.txt\"}"
+                + "},"//
+                + "\"analyzer\":{"
+                + "\"ja_analyzer\":{\"type\":\"custom\",\"tokenizer\":\"reloadable_kuromoji_neologd_tokenizer\"},"
+                + "\"ja_concat_analyzer\":{\"type\":\"custom\",\"tokenizer\":\"reloadable_kuromoji_neologd_tokenizer\",\"filter\":[\"tag_concat_filter\"]}"
+                + "}"//
+                + "}}}";
+        runner.createIndex(index,
+                Settings.builder().loadFromSource(indexSettings)
+                        .build());
+        runner.ensureYellow();
+
+        {
+            String text = "詳細設計";
+            try (CurlResponse response = Curl
+                    .post(node, "/" + index + "/_analyze")
+                    .param("analyzer", "ja_concat_analyzer").body(text)
+                    .execute()) {
+                @SuppressWarnings("unchecked")
+                List<Map<String, Object>> tokens = (List<Map<String, Object>>) response
+                        .getContentAsMap().get("tokens");
+                assertEquals(1, tokens.size());
+                assertEquals("詳細設計", tokens.get(0).get("token").toString());
+            }
+        }
+    }
+    @Test
+    public void test_basic2() throws Exception {
+
+        runner.ensureYellow();
+        Node node = runner.node();
+
+        final String index = "dataset";
+
+        final String indexSettings = "{\"index\":{\"analysis\":{"
+                + "\"filter\":{"
+                + "\"tag_concat_filter\":{\"type\":\"kuromoji_neologd_pos_concat\",\"tags\":[\"名詞-形容動詞語幹\",\"名詞-サ変接続\"]}"
+                + "},"//
+                + "\"analyzer\":{"
+                + "\"ja_analyzer\":{\"type\":\"custom\",\"tokenizer\":\"reloadable_kuromoji_neologd_tokenizer\"},"
+                + "\"ja_concat_analyzer\":{\"type\":\"custom\",\"tokenizer\":\"reloadable_kuromoji_neologd_tokenizer\",\"filter\":[\"tag_concat_filter\"]}"
+                + "}"//
+                + "}}}";
+        runner.createIndex(index,
+                Settings.builder().loadFromSource(indexSettings)
+                        .build());
+        runner.ensureYellow();
+
+        {
+            String text = "詳細設計";
+            try (CurlResponse response = Curl
+                    .post(node, "/" + index + "/_analyze")
+                    .param("analyzer", "ja_concat_analyzer").body(text)
+                    .execute()) {
+                @SuppressWarnings("unchecked")
+                List<Map<String, Object>> tokens = (List<Map<String, Object>>) response
+                        .getContentAsMap().get("tokens");
+                assertEquals(1, tokens.size());
+                assertEquals("詳細設計", tokens.get(0).get("token").toString());
+            }
+        }
+    }
+
+    private void updateDictionary(File file, String content)
+            throws IOException, UnsupportedEncodingException,
+            FileNotFoundException {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(file), "UTF-8"))) {
+            bw.write(content);
+            bw.flush();
+        }
+    }
+}
